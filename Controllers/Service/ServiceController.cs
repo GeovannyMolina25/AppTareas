372d6f9ccd4eb61dad913b411cc0cecba9978f21
Nelson Molina <geovannysangucho25@gmail.com>
2024-12-04 21:13:21 -0500

    Area servicios agregada

---
+++ Controllers/Service/ServiceController.cs
@@ -0,0 +1,16 @@
+﻿using AppTareas.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppTareas.Controllers.Service
+{
+    public class ServiceController : Controller
+    {
+        private readonly ServiceContext _db;
+        
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ Service/ServiceService.cs
@@ -0,0 +1,26 @@
+﻿using AppTareas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace AppTareas.Service
+{
+    public class ServiceService : Controller
+    {
+        private readonly ServiceContext _db;
+        
+        public ServiceService(ServiceContext db)
+        {
+            _db = db;
+        }
+        public List<Servicio> ObtenerServicios()
+        {
+            var idUsuarioClaim = User.Claims.FirstOrDefault(c => c.Type == "IdUsuario");
+            int idUsuario = int.Parse(idUsuarioClaim.Value);
+            var datos = _db.Servicios
+                .Where(e => e.IdUsuario == idUsuario)
+                .ToList();
+            return datos;
+        }
+    }
+}
