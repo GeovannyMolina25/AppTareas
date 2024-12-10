f700f74f447608968d828a568219d6f1e9f1cc37
Nelson Molina <geovannysangucho25@gmail.com>
2024-12-09 23:57:34 -0500

    Greacion de add para el los servicios

---
+++ Models/View models/TiposServiciosViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace AppTareas.Models.View_models
+{
+    public class TiposServiciosViewModel
+    {
+        public int IdUsuario { get; set; }
+        public int IdTipoServicio { get; set; }
+
+        public string Titulo { get; set; } = null!;
+        public string NombreTipo { get; set; } = null!;
+        public string? Descripcion { get; set; }
+        public decimal Presupuesto { get; set; }
+        public int? HorasEstimadas { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public string Estado { get; set; } = null!;
+
+    }
+}
--- obj/Debug/net6.0/AppTareas.AssemblyInfo.cs
+++ obj/Debug/net6.0/AppTareas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppTareas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7f79377770d7d81e4a4f8067e6545b3f4a7b01d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+372d6f9ccd4eb61dad913b411cc0cecba9978f21")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppTareas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppTareas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
